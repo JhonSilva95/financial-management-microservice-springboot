0ba69bca34eda4dd4f9a6a4c387a33481a399aaa
Jhon Silva <jhon.silva@fonyou.com>
2023-11-29 12:03:31 -0500

    Initial commit code

---
+++ src/main/java/financial/management/microservice/springboot/FinancialManagementMicroserviceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package financial.management.microservice.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialManagementMicroserviceSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialManagementMicroserviceSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/financial/management/microservice/springboot/FinancialManagementMicroserviceSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package financial.management.microservice.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementMicroserviceSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
